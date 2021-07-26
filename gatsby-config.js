2a014f19fb9164c8197852823e553f73c099129d
tommynov1000 <tommynov1000@GMAIL.com>
2021-07-26 13:54:58 -0400

    Logo and Icon updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
       'Thomas Taylor is a SRE who specializes in automation and reliability.',
     siteUrl: 'https://thomastaylor.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
